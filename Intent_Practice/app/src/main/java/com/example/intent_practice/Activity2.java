db14670e6e448a9746053f4629fa411765e18edb
Kashif Tariq <kashift99@gmail.com>
2021-04-01 21:07:48 +0500

    Intent Practice added

--- Intent_Practice/app/src/main/java/com/example/intent_practice/Activity2.java
+++ Intent_Practice/app/src/main/java/com/example/intent_practice/Activity2.java
@@ -2,7 +2,10 @@ package com.example.intent_practice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -11,4 +14,22 @@ public class Activity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
     }
+    public void goToActivity1(View view) {
+        Intent intent= new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void call(View view) {
+        Uri uri= Uri.parse("tel:+9209417575");
+        Intent intent= new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(intent);
+
+    }
+
+    public void goToSite(View view) {
+        Uri uri= Uri.parse("https://github.com/kashifisonly1/Mobile_Computing_Progress");
+        Intent intent= new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(intent);
+    }
+
 }
--- Intent_Practice/app/src/main/java/com/example/intent_practice/MainActivity.java
+++ Intent_Practice/app/src/main/java/com/example/intent_practice/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.intent_practice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void goToActivity2(View view) {
+        Intent intent= new Intent(this, Activity2.class);
+        startActivity(intent);
     }
 }
