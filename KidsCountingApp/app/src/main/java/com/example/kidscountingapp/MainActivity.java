a422ed32b267f8d288f43f747556fbdd1c0a679d
kashifisonly1 <kashift99@gmail.com>
2021-04-07 02:35:19 +0500

    Play button added

--- KidsCountingApp/app/src/main/java/com/example/kidscountingapp/MainActivity.java
+++ KidsCountingApp/app/src/main/java/com/example/kidscountingapp/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.kidscountingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mediaPlayer= MediaPlayer.create(this,R.raw.bg);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
+    }
+
+    public void startApp(View view) {
+        Intent intent= new Intent(this, PlayCounting.class);
+        startActivity(intent);
     }
 }
---
+++ KidsCountingApp/app/src/main/java/com/example/kidscountingapp/PlayCounting.java
@@ -0,0 +1,14 @@
+package com.example.kidscountingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlayCounting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_counting);
+    }
+}
