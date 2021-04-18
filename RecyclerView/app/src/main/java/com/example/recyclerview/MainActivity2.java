6f45c1315ae0a8a916f993190f6c486088e88f30
kashifisonly1 <41770657+kashifisonly1@users.noreply.github.com>
2021-04-18 19:26:16 +0500

    second activity added

--- RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity2.java
+++ RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity2.java
@@ -3,6 +3,8 @@ package com.example.recyclerview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,7 +12,9 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        getIntent().getIntExtra("img");
-        getIntent().getIntExtra()
+        ((ImageView)findViewById(R.id.mainImageView)).setImageResource(getIntent().getIntExtra("img", 0));
+        ((TextView)findViewById(R.id.title)).setText(getIntent().getStringExtra("name"));
+        ((TextView)findViewById(R.id.description)).setText(getIntent().getStringExtra("description"));
+
     }
 }
