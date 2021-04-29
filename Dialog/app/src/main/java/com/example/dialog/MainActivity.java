113587b7395742c98906aa346dc08b5f36c22b06
kashifisonly1 <41770657+kashifisonly1@users.noreply.github.com>
2021-04-29 22:37:54 +0500

    dialog layout added

--- Dialog/app/src/main/java/com/example/dialog/MainActivity.java
+++ Dialog/app/src/main/java/com/example/dialog/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.dialog;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +18,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void LoadDialog(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+        builder.setTitle("Dialog Title");
+        builder.setMessage("It is message of my Dialog");
+
+        builder.setCancelable(false);
+        LayoutInflater inflater=this.getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.dialog,null));
+
+        builder.setPositiveButton("Positive",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "POSITIVE CLICKED", Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+        builder.setNegativeButton("Negative",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "NEGATIVE CLICKED", Toast.LENGTH_LONG).show();
+
+                    }
+                }
+        );
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
