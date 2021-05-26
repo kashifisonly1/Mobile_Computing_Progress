2bfebfc2c4bef3752e84bff52b4535d4165ba439
kashifisonly1 <41770657+kashifisonly1@users.noreply.github.com>
2021-05-26 17:08:04 +0500

    fragment 2 click event added

--- Fragment/app/src/main/java/com/example/fragment/Fragment2.java
+++ Fragment/app/src/main/java/com/example/fragment/Fragment2.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Fragment2 extends Fragment {
 
@@ -28,6 +30,14 @@ public class Fragment2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_2, container, false);
+        View view = inflater.inflate(R.layout.fragment_2, container, false);
+        Button btn = view.findViewById(R.id.frag2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "THIS IS TOAST FROM FRAG@", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
     }
 }
