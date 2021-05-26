5411e5902386beba9acf5d0d2476456a31b5a046
kashifisonly1 <41770657+kashifisonly1@users.noreply.github.com>
2021-05-26 17:01:33 +0500

    fragment 2 include

---
+++ Fragment/app/src/main/java/com/example/fragment/Fragment2.java
@@ -0,0 +1,33 @@
+package com.example.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment2 extends Fragment {
+
+    public Fragment2() {
+    }
+
+    public static Fragment2 newInstance(String param1, String param2) {
+        Fragment2 fragment = new Fragment2();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_2, container, false);
+    }
+}
