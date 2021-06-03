706112a05b70961b3a87b84597d5a0d0082bc63a
kashifisonly1 <41770657+kashifisonly1@users.noreply.github.com>
2021-06-03 14:26:40 +0500

    code copied online for notification

--- NotificationProject/app/src/main/java/com/example/notificationproject/MainActivity.java
+++ NotificationProject/app/src/main/java/com/example/notificationproject/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.notificationproject;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @RequiresApi(api= Build.VERSION_CODES.O)
+    public void createNotification(View view) {
+
+        int notificationId = 1;
+        String channelId="channel_1";
+        CharSequence name = getString(R.string.app_name);
+        int importance = NotificationManager.IMPORTANCE_HIGH;
+        NotificationChannel notificationChannel=new NotificationChannel(channelId,name,importance);
+        Notification notification= new Notification.Builder(MainActivity.this)
+                .setContentText("New Message")
+                .setContentText("You have new messages")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setChannelId(channelId)
+                .build();
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(notificationChannel);
+        notificationManager.notify(notificationId,notification);
     }
 }
